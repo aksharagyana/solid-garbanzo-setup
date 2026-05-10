082d83f44871a4e651bcfd6c3a39d293a00332df
aksharagyana <93100395+aksharagyana@users.noreply.github.com>
2026-05-11 00:40:12 +0100

    up scripts

---
+++ onlocal/ai.sh
@@ -0,0 +1,7 @@
+graphify_setup(){
+    graphify codex install
+    graphify copilot install
+    graphify cursor install
+    graphify antigravity install
+    graphify update . --mode deep
+}
---
+++ onlocal/setup.sh
@@ -0,0 +1,6 @@
+setup_path="/Users/sandeepkotha/code/macsetup"
+
+source "${setup_path}/env.sh"
+source "${setup_path}/docker_env.sh"
+source "${setup_path}//docker_util.sh"
+source "${setup_path}/ai.sh"
