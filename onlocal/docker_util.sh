da0b1b094076024ec94cb2e55418143f63f12aaf
aksharagyana <93100395+aksharagyana@users.noreply.github.com>
2026-05-11 01:12:34 +0100

    update script

--- onlocal/docker_util.sh
+++ onlocal/docker_util.sh
@@ -3,19 +3,19 @@ gchr_login(){
 }
 
 dev_go(){
-    docker run -it --pull=always --env-file "${DOCKER_ENV_FILE}" -v "${CODE_ON_MAC}":"${CODE_ON_CONT}" "${GO_IMAGE}" bash
+    docker run -it --pull=always --env-file "${DOCKER_ENV_FILE}" -v "${CODE_ON_MAC}":"${CODE_ON_CONT}" -v "${UTILS_ON_MAC}":"${UTILS_ON_CONT}" "${GO_IMAGE}" bash
 }
 
 dev_python(){
-    docker run -it --pull=always --env-file "${DOCKER_ENV_FILE}" -v "${CODE_ON_MAC}":"${CODE_ON_CONT}" "${PYTHON_IMAGE}" bash
+    docker run -it --pull=always --env-file "${DOCKER_ENV_FILE}" -v "${CODE_ON_MAC}":"${CODE_ON_CONT}" -v "${UTILS_ON_MAC}":"${UTILS_ON_CONT}" "${PYTHON_IMAGE}" bash
 }
 
 dev_node(){
-    docker run -it --pull=always --env-file "${DOCKER_ENV_FILE}" -p 3000:3000 -p 7007:7007 -p 5173:5173  -v /var/run/docker.sock:/var/run/docker.sock -v "${CODE_ON_MAC}":"${CODE_ON_CONT}" "${DEBIAN_IMAGE}" bash
+    docker run -it --pull=always --env-file "${DOCKER_ENV_FILE}" -p 3000:3000 -p 7007:7007 -p 5173:5173  -v /var/run/docker.sock:/var/run/docker.sock -v "${CODE_ON_MAC}":"${CODE_ON_CONT}" -v "${UTILS_ON_MAC}":"${UTILS_ON_CONT}" "${DEBIAN_IMAGE}" bash
 }
 
 dev_debian(){
-    docker run -it --pull=always --env-file "${DOCKER_ENV_FILE}" -v "${CODE_ON_MAC}":"${CODE_ON_CONT}" "${DEBIAN_IMAGE}" bash
+    docker run -it --pull=always --env-file "${DOCKER_ENV_FILE}" -v "${CODE_ON_MAC}":"${CODE_ON_CONT}" -v "${UTILS_ON_MAC}":"${UTILS_ON_CONT}" "${DEBIAN_IMAGE}" bash
 }
 
 docker_cleanup(){
@@ -45,4 +45,3 @@ docker_cleanup(){
 
     echo -e "\nAll Docker resources have been aggressively deleted."
 }
-
