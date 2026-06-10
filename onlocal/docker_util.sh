53d249a32919aed1589a3211d00daae69a1a582e
aksharagyana <93100395+aksharagyana@users.noreply.github.com>
2026-06-10 14:17:36 +0100

    Change image from DEBIAN to NODE in dev_node function

--- onlocal/docker_util.sh
+++ onlocal/docker_util.sh
@@ -11,7 +11,7 @@ dev_python(){
 }
 
 dev_node(){
-    docker run -it --pull=always --env-file "${DOCKER_ENV_FILE}" -p 3000:3000 -p 7007:7007 -p 5173:5173  -v /var/run/docker.sock:/var/run/docker.sock -v "${CODE_ON_MAC}":"${CODE_ON_CONT}" -v "${UTILS_ON_MAC}":"${UTILS_ON_CONT}" "${DEBIAN_IMAGE}" bash
+    docker run -it --pull=always --env-file "${DOCKER_ENV_FILE}" -p 3000:3000 -p 7007:7007 -p 5173:5173  -v /var/run/docker.sock:/var/run/docker.sock -v "${CODE_ON_MAC}":"${CODE_ON_CONT}" -v "${UTILS_ON_MAC}":"${UTILS_ON_CONT}" "${NODE_IMAGE}" bash
 }
 
 dev_debian(){
